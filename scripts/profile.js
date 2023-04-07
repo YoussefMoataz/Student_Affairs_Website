0e9632994a10dd05833d942d50c57c92ae9a4deb
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-04-07 14:54:19 +0200

    Added student class. Student class with submitted form.

--- scripts/profile.js
+++ scripts/profile.js
@@ -6,3 +6,33 @@
         -Show profile page
         -Edit profile page
 */
+
+function formSubmitted(){
+
+    var id = document.getElementById("studentID").value;
+    var name = document.getElementById("studentName").value;
+    var dateOfBirth = document.getElementById("studentBirthDate").value;
+    var gpa = document.getElementById("studentGPA").value;
+    var level = document.getElementById("studentLevel").value;
+    var department = document.getElementById("studentDepartment").value;
+    var email = document.getElementById("studentEmail").value;
+    var phone = document.getElementById("studentPhone").value;
+
+    var gender = "";
+    var radios = document.getElementsByName("studentGender");
+    for (var i = 0; i < 2; i++) {
+        if (radios[i].checked) {
+            gender = radios[i].value;
+            break;
+        }
+    }
+
+    var status = document.getElementById("studentStatus").checked;
+    
+    let student = new Student(id, name, dateOfBirth, gpa, level, department, email, phone, gender, status);
+
+    // alert(JSON.stringify(student));
+
+    // alert(id + name + dateOfBirth + gpa + level + department + email + phone + gender + status );
+
+}
---
+++ scripts/student.js
@@ -0,0 +1,16 @@
+class Student{
+
+    constructor(Id, Name, DateOfBirth, GPA, Level, Department, Email, Phone, Gender, Status){
+        this.id = Id;
+        this.name = Name;
+        this.dateOfBirth = DateOfBirth;
+        this.gpa = GPA;
+        this.level = Level;
+        this.department = Department;
+        this.email = Email;
+        this.phone = Phone;
+        this.gender = Gender;
+        this.status = Status;
+    }
+
+};
