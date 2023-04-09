c9b24d6834dd58f51d06dd03cff52399897c3fbb
Ibn-Maged <mohamedmaged1607@gmail.com>
2023-04-09 22:27:28 +0200

    changed redirection path

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,5 @@
-let user_name = document.getElementById("userName");
-let password = document.getElementById("password");
 let error_msg = "Wrong Credentials. Please Try Again.";
-let submit_btn = document.getElementById("loginSubmitBtn");
 let form = document.getElementById("loginForm");
-
-submit_btn.addEventListener("click", validate_input);
 
 function validate_input()
 {
@@ -12,13 +7,13 @@ function validate_input()
     let password = form.password.value;
     if(user_name == "admin" && password == "admin")
     {
-        window.location.href = '/index.html';
+        window.location.href = '/home.html';
         alert("you have logged in successfully");
     }
     else
     {
         // window.location.reload();
         // document.getElementById("error-message").innerHTML = error_msg;
-        alert("Wrong Credentials. Please Try Again.");ss
+        alert("Wrong Credentials. Please Try Again.")
     }
 }
