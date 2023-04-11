efc8006b8faba3c1e53e0496c241b63a910869b4
NourhanMahmoudd <nourhanidc@gmail.com>
2023-04-11 15:40:21 +0200

    Editing in css &html, adding js

---
+++ scripts/department.js
@@ -0,0 +1,56 @@
+let storage = window.localStorage;
+
+let students = JSON.parse(storage.getItem("StudentsData"));
+let StudentIndex = storage.getItem("StudentIndex");
+let studentToSetDepartment = JSON.parse(students[StudentIndex]);
+
+function assignDepartment(){
+ 
+  document.getElementById("SID").innerHTML = studentToSetDepartment.id;
+  document.getElementById("SName").innerHTML = studentToSetDepartment.name;
+  document.getElementById("SDepartment").innerHTML = studentToSetDepartment.department;
+
+}
+
+function Submitted() {
+
+  // Check if student's level is 3
+  if (studentToSetDepartment.level != 3) {
+
+    alert("Error: This action is only applicable for students at level 3.");
+    return;
+  
+  }
+
+  else{
+
+    //Get the selected department from the dropdown list
+    let departmentSelect = document.getElementsByName("Department")[0];
+    let selectedDepartment = departmentSelect.options[departmentSelect.selectedIndex].text;
+    //no selection
+    if (selectedDepartment == "Select a department") {
+      alert("Error: Please choose a department.");
+      return;
+    }
+    else{
+
+    // Set the department of the student
+    studentToSetDepartment.department = selectedDepartment;
+    students[StudentIndex] = JSON.stringify(studentToSetDepartment);
+    storage.setItem("StudentsData", JSON.stringify(students));
+
+    alert("Department has been set successsfully!");
+
+    goToProfile();
+
+    }
+
+  }
+
+}
+
+function goToProfile(){
+
+    window.location.href = "show_profile.html";
+
+}
