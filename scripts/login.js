93d982e50bf4d219dfd51bdf8c722a8d78344b83
Ibn-Maged <mohamedmaged1607@gmail.com>
2023-04-13 16:45:56 +0200

    Finished the Log in Authorization Function
    - There are Currently 2 Users Available for Testing in the Local Storage:
        - name: maged   password: 1234
        - name: youssef password: 5678
    - After Authorization, the Current User is Stored in the Local Storage with the key "currentUser"
    - admin admin Credentials are Invalid Now

--- scripts/login.js
+++ scripts/login.js
@@ -1,16 +1,26 @@
 let error_msg = "Wrong Credentials. Please Try Again.";
 let form = document.getElementById("loginForm");
+let usersArr = [];
+storage.setItem("currentUser", "undefined");
+usersArr = JSON.parse(storage.getItem("UsersData"));
 
 function validate_input()
 {
     let user_name = form.userName.value;
     let password = form.password.value;
-    if(user_name == "admin" && password == "admin")
+    let current_user;
+    let found = false;
+    for(let i = 0; i < usersArr.length; i++)
     {
-        window.location.href = '/home.html';
-        // alert("you have logged in successfully");
+        current_user = JSON.parse(usersArr[i]);
+        if(user_name == current_user["name"] && password == current_user["password"])
+        {
+            storage.setItem("currentUser", JSON.stringify(current_user));
+            window.location.href = '/home.html';
+            found = true;
+        }
     }
-    else
+    if(!found)
     {
         document.getElementById("error-message").innerHTML = error_msg;
         document.getElementById("userName").style.borderColor = "red";
