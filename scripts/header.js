876430502cfb519324a03edb43defda7ebff9959
ahmedsaad123456 <102562587+ahmedsaad123456@users.noreply.github.com>
2023-04-14 16:01:15 +0200

    Add new css on sidebar

--- scripts/header.js
+++ scripts/header.js
@@ -15,3 +15,31 @@ element.innerHTML = newelment.innerHTML;
 if(window.location.href.includes("show_profile.html")){
     document.getElementById("profilePage").style.display = "none";
 }
+
+
+if(window.location.href.includes("home.html")){
+    document.getElementById("home").style.backgroundColor= "var(--main-background-color)";
+    document.getElementById("home").style.color="black";
+}
+
+
+if(window.location.href.includes("add_user.html")){
+    document.getElementById("add_user").style.backgroundColor= "var(--main-background-color)";
+    document.getElementById("add_user").style.color="black";
+}
+
+
+if(window.location.href.includes("view_user.html")){
+    document.getElementById("view_user").style.backgroundColor= "var(--main-background-color)";
+    document.getElementById("view_user").style.color="black";
+}
+
+
+if(window.location.href.includes("all_students.html")){
+    document.getElementById("all_students").style.backgroundColor= "var(--main-background-color)";
+    document.getElementById("all_students").style.color="black";
+}
+
+
+
+
