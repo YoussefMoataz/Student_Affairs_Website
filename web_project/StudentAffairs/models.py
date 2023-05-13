c11c39cf430452788e37c13322ad6f165aea6cf4
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-05-13 08:18:59 +0300

    Added Student database model

--- web_project/StudentAffairs/models.py
+++ web_project/StudentAffairs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    id = models.CharField(name="id", primary_key=True, max_length=8)
+    name = models.CharField(name="name", max_length=100)
+    dateOfBirth = models.DateField(name="dateOfBirth")
+    gpa = models.DecimalField(name="gpa", max_digits=3, decimal_places=2)
+    level = models.IntegerField(name="level")
+    department = models.CharField(name="department", max_length=2)
+    email = models.EmailField(name="email")
+    phone = models.CharField(name="phone", max_length=12)
+    gender = models.BooleanField(name="gender")
+    status = models.BooleanField(name="status")
+
+    def __str__(self):
+        return self.id
--- web_project/StudentAffairs/urls.py
+++ web_project/StudentAffairs/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.show_profile, name="index"),
+    path("profile/", views.show_profile, name="ShowProfile"),
 ]
