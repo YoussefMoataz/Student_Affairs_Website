56ed34afeb249b50b2ef92d2b16d743607e8d05d
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-05-14 17:23:39 +0300

    Updated project URLs. Updated profile URLs to match old ones.

--- web_project/StudentAffairs/urls.py
+++ web_project/StudentAffairs/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="Index"),
-    path("profile/", views.add_profile, name="AddProfile"),
-    path("profile/<int:st_id>", views.show_profile, name="ShowProfile"),
-    path("profile/update/<int:st_id>", views.update_profile, name="UpdateProfile"),
-    path("profile/delete/<int:st_id>", views.delete_profile, name="DeleteProfile"),
+    path("app/profile/", views.add_profile, name="AddProfile"),
+    path("app/profile/<int:st_id>", views.show_profile, name="ShowProfile"),
+    path("app/profile/update/<int:st_id>", views.update_profile, name="UpdateProfile"),
+    path("app/profile/delete/<int:st_id>", views.delete_profile, name="DeleteProfile"),
 ]
