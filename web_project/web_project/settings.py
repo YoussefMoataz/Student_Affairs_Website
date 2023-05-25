bea2e895bf51590737bc41240f3a6af35c6e136e
Ibn-Maged <mohamedmaged1607@gmail.com>
2023-05-25 11:36:48 +0300

    Removed Changes in Settings.py

--- web_project/web_project/settings.py
+++ web_project/web_project/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = (
     'StudentAffairs/static',
 )
 
-STATICFILES_DIRS=[(os.path.join(BASE_DIR,'StudentAffairs/static'))]
+STATICFILES_DIRS=['StudentAffairs/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
